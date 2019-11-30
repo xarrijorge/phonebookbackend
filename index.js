a8407c2f212a10acdbecf0affedc549cd09e67a8
Xarri Jorge <kodinglife@gmail.com>
2019-11-30 11:14:03 +0000

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
+app.use(cors());
 app.use(bodyParser.json());
 
 morgan.token("body", function(req, res) {
