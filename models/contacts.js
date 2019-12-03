6982061c856a70b640829bd7c789222b2eb11613
Xarri Jorge <kodinglife@gmail.com>
2019-12-03 21:52:50 +0000

    Adding String method to mongoose URI

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
 mongoose
-  .connect(url, { useNewUrlParser: true })
+  .connect(String(url), { useNewUrlParser: true })
   .then(result => {
     console.log("connected to", url);
   })
