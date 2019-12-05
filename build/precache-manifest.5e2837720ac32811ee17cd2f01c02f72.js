f7509cc0d180a134c0a55d38faf2d057db84de14
Xarri Jorge <kodinglife@gmail.com>
2019-12-05 05:35:47 +0000

    uibuild

---
+++ build/precache-manifest.5e2837720ac32811ee17cd2f01c02f72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadb4ae1b28d4442e1d2d2d4064e6773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f88271987232cbd2ed",
+    "url": "/static/css/main.bb6e4da9.chunk.css"
+  },
+  {
+    "revision": "0705d1fea8e49a586295",
+    "url": "/static/js/2.d0857bc7.chunk.js"
+  },
+  {
+    "revision": "97f88271987232cbd2ed",
+    "url": "/static/js/main.9c7eafd0.chunk.js"
+  },
+  {
+    "revision": "df99deec2933845bfe71",
+    "url": "/static/js/runtime-main.b4276010.js"
+  }
+]);
